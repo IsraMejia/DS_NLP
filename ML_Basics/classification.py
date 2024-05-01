dd2ca12fbeb15876f861d6eaeab1f5d04f9efc27
Isra Mejia <ihmaisrael@gmail.com>
2024-05-01 00:45:05 -0600

    hasta aqui llegue mcqueen mañana le sigo con the preparation of the training datasets

---
+++ ML_Basics/classification.py
@@ -0,0 +1,8 @@
+ 
+import numpy as np
+from sklearn.datasets import load_breast_cancer #Breast cancer dataset (dataset de cancer de mama)
+
+data = load_breast_cancer() #Loading the dataset
+
+type(data) #Checking the type of the dataset
+
